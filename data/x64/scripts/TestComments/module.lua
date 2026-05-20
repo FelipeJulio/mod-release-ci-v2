3ab545cedd859624904dde045dc96ee5f6f05b9f
Feju <felipe_julio@live.com>
2026-05-20 19:37:22 -0300

    feat: fixed path strip depht

---
+++ data/x64/scripts/TestComments/module.lua
@@ -0,0 +1,7 @@
+-- Made by ABCDE
+local function foo()
+    -- comment 
+    return 1 + 2 -- comment 
+end
+
+return foo
---
+++ data/x64/scripts/config/TestComments/config.lua
@@ -0,0 +1,7 @@
+-- Made by ABCDE
+return {
+    param1 = 1, -- comment 1
+    param2 = 2, -- comment 2
+    param3 = 3, -- comment 3
+    param4 = 4 -- comment 4
+}
